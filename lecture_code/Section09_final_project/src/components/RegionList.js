f4ed2c8f6a5a6732645a0386c297f7b523f0cb28
JisooPyo <audwl326@gmail.com>
2025-12-23 22:34:12 +0900

    DAY13
    9-51. Header 개발
    9-52. RegionList 개발

--- lecture_code/Section09_final_project/src/components/RegionList.js
+++ lecture_code/Section09_final_project/src/components/RegionList.js
@@ -1,12 +1,53 @@
-export default function RegionList() {
+export default function RegionList({ $app, initialState, handleRegion }) {
+  this.state = initialState;
+
   this.$target = document.createElement("div");
   this.$target.className = "region-list";
 
-  this.template = () => {};
-  this.render = () => {};
+  this.handleRegion = handleRegion;
+  $app.appendChild(this.$target);
+
+  this.template = () => {
+    const regionList = [
+      "🚀 All",
+      "🌏 Asia",
+      "🕌 Middle-East",
+      "🇪🇺 Europe",
+      "💃 Latin-America",
+      "🐘 Africa",
+      "🏈 North-America",
+      "🏄 Oceania",
+    ];
+    let temp = ``;
+    regionList.forEach((elm) => {
+      let regionId = elm.split(" ")[1];
+      temp += `<div id=${regionId}>${elm}</div>`;
+    });
+
+    return temp;
+  };
+
+  this.render = () => {
+    this.$target.innerHTML = this.template();
+    if (this.state) {
+      let $currentRegion = document.getElementById(this.state);
+      $currentRegion && $currentRegion.classList.add("clicked");
+    } else {
+      document.getElementById("All").classList.add("clicked");
+    }
+
+    const $regionList = this.$target.querySelectorAll("div");
+    $regionList.forEach((elm) => {
+      elm.addEventListener("click", () => {
+        this.handleRegion(elm.id);
+      });
+    });
+  };
+
   this.setState = (newState) => {
     this.state = newState;
     this.render();
   };
+
   this.render();
 }
