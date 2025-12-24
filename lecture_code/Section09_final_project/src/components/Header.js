3cdf7e4006a56983653bf5f42d13a30e6d7beab6
JisooPyo <audwl326@gmail.com>
2025-12-24 23:46:42 +0900

    DAY14

--- lecture_code/Section09_final_project/src/components/Header.js
+++ lecture_code/Section09_final_project/src/components/Header.js
@@ -14,11 +14,13 @@ export default function Header({
   $app.appendChild(this.$target);
 
   this.template = () => {
-    const { sortBy, searchWord } = this.state;
+    const { sortBy, searchWord, currentPage } = this.state;
     let temp = `
     <div class="title">
       <a href="/">✈️ Trip Wiki</a>
-    </div>
+    </div>`;
+    if (!currentPage.includes("/city/")) {
+      temp += `
     <div class="filter-search-container">
       <div class="filter">
         <select id="sortList" class="sort-list">
@@ -47,21 +49,26 @@ export default function Header({
           <input type="text" placeholder="Search" id="search" autocomplete="off" value=${searchWord} >
       </div>
     </div>`;
+    }
 
     return temp;
   };
 
   this.render = () => {
     this.$target.innerHTML = this.template();
-    document.getElementById("sortList").addEventListener("change", (event) => {
-      this.handleSortChange(event.target.value);
-    });
-    const $searchInput = document.getElementById("search");
-    document.getElementById("search").addEventListener("keydown", (event) => {
-      if (event.key === "Enter") {
-        this.handleSearch($searchInput.value);
-      }
-    });
+    if (!this.state.currentPage.includes("/city/")) {
+      document
+        .getElementById("sortList")
+        .addEventListener("change", (event) => {
+          this.handleSortChange(event.target.value);
+        });
+      const $searchInput = document.getElementById("search");
+      document.getElementById("search").addEventListener("keydown", (event) => {
+        if (event.key === "Enter") {
+          this.handleSearch($searchInput.value);
+        }
+      });
+    }
   };
 
   this.setState = (newState) => {
